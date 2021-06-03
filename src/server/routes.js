e1bae70c33692965f49960b1c27b658327bc893d
Juan Luis Faylon <juanluisfaylon@Juans-MacBook-Pro.local>
2021-06-03 18:20:36 +0800

    chore: change readme.md

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const multer = require("multer");
 
 const router = express.Router();
-
-const upload = multer();
 
 const productController = require("./controllers/product");
 
 router.get("/", productController.test);
 router.get("/products", productController.getAllProducts);
-router.post("/products", upload.none(), productController.createProduct);
+router.post("/products", productController.createProduct);
 router.get("/products/:id", productController.getProduct);
-router.post("/products/:id", upload.none(), productController.editProduct);
+router.post("/products/:id", productController.editProduct);
 router.delete("/products/:id", productController.deleteProduct);
 
 module.exports = router;
